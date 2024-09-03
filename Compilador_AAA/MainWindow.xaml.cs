d61480bd42b1fa90e318212f2f29a8f9f76fbc84
Alejandro Andaverde Del Angel <a2203720081@alumnos.uat.edu.mx>
2024-09-03 08:04:27 -0500

    Agregar archivos de proyecto.

---
+++ Compilador_AAA/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Compilador_AAA
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+            EditorLV.Items.Add("hehe");
+            EditorLV.Items.Add("hehe");
+            EditorLV.Items.Add("hehe");
+            EditorLV.Items.Add("hehe");
+            EditorLV.Items.Add("hehe");
+            LinesLV.Items.Add("1");
+            LinesLV.Items.Add("2");
+            LinesLV.Items.Add("3");
+            LinesLV.Items.Add("4");
+            LinesLV.Items.Add("5");
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hwn, int wMsg, int wParam, int lParam);
+        private void WindowBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
+        }
+        private void WindowBar_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+                this.WindowState = WindowState.Maximized;
+            else
+                this.WindowState = WindowState.Normal;
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+    }
+}
